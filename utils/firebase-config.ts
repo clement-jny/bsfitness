9b6b0ce71dde62667be1207b33a9a40e2c35ec38
clement-jny <clementjaunay@free.fr>
2025-02-03 23:59:09 +0100

    feat: init auth with RN AsyncStorage

--- utils/firebase-config.ts
+++ utils/firebase-config.ts
@@ -1,10 +1,11 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth } from 'firebase/auth';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your web app's Firebase configuration
 const config = {
@@ -18,7 +19,12 @@ const config = {
 
 // Initialize Firebase
 const app = initializeApp(config);
-const auth = getAuth(app);
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
 const db = getFirestore(app);
+
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
 
 export const firebaseConfig = { auth, db };
