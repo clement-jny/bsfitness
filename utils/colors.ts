479f45f0060913d6ffed5711e2fcd9bf5051541d
clement-jny <clementjaunay@free.fr>
2025-02-04 00:00:06 +0100

    chore: update all colors and add types

--- utils/colors.ts
+++ utils/colors.ts
@@ -1,24 +1,64 @@
-const light = {
-  primary: '#FF6347',
-  secondary: '#FFA07A',
-  white: '#FFF',
-  black: '#000',
-  green: '#33BBB5',
+type TBaseColor = {
+  primary: string;
+  secondary: string;
+  accent: string;
 
-  text: '#0f0804',
-  background: '#EBE5E0',
-  accent: '#90BC81',
+  white: string;
+  black: string;
+  grey: string;
+  orange: string;
 };
-const dark = {
-  primary: '#FF6347',
-  secondary: '#FFA07A',
-  white: '#FFF',
-  black: '#000',
-  green: '#33BBB5',
 
-  text: '#fbf3ef',
-  background: '#1f1914',
-  accent: '#b05107',
+type TColor = TBaseColor & {
+  text: string;
+  background: string;
+  title?: string;
+
+  selected: string;
 };
+
+const baseColor: TBaseColor = {
+  primary: '#34bcb5',
+  secondary: '#1F2E45', // lighter ?
+  accent: '#e84579',
+
+  white: '#fff',
+  black: '#000',
+  grey: '#9dafae',
+  orange: '#ffac38',
+};
+
+const light: TColor = {
+  ...baseColor,
+
+  text: '#030c0c',
+  background: '#ecf3f2', // #fdfeff
+  // title: '',
+
+  selected: '#f0f5f5',
+};
+const dark: TColor = {
+  ...baseColor,
+
+  text: '#f3fcfc',
+  background: '#0c1312',
+  // title: '',
+
+  selected: '#0a0f0f',
+};
+// Old dark color
+// primary: '#43cbc5',
+// secondary: '',
+// accent: '#ba174b',
+// white: '#fff',
+// black: '#000',
+// grey: '#506261',
+// orange: '#c77400',
 
 export const colors = { light, dark };
+
+// #1F2E45 // ! (x2) title ? titleCard, titleSection, titleCourse, titleTab ==== secondary
+// #DDCAD9
+// #484D6D
+// #004F2D
+// #083D77
