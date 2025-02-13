7c6d3da4ad3e2736e3ed2a1025d5d7091ac91209
clement-jny <clementjaunay@free.fr>
2025-02-13 21:16:28 +0100

    feat(type): update update-user type

--- types/user/update-user.dto.ts
+++ types/user/update-user.dto.ts
@@ -1,9 +1,5 @@
 import { TNewUserDTO } from './new-user.dto';
 
-// type TUpdateUserDTO = Partial<TNewUserDTO> & {
-//   docUid: string; // ID Firestore du document utilisateur (uid2)
-// };
-type TUpdateUserDTO = Partial<TNewUserDTO> & {};
-// type TUpdateUserDTO = Partial<Omit<TNewUserDTO, 'authUid' | 'email'>> & {};
+type TUpdateUserDTO = Partial<Omit<TNewUserDTO, 'authUid' | 'email'>> & {};
 
 export { TUpdateUserDTO };
