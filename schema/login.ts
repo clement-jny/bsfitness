965c786cbf90b8fe232cf2c4772b82455f2b6a13
clement-jny <clementjaunay@free.fr>
2025-02-17 23:01:43 +0100

    refactor: update type and schemas

--- schema/login.ts
+++ schema/login.ts
@@ -1,11 +1,13 @@
 import { z } from 'zod';
 
 const loginSchema = z.object({
-  email: z.string().email({ message: 'Invalid email address' }).trim(),
+  email: z
+    .string({ required_error: 'Email is required' })
+    .email({ message: 'Invalid email address' })
+    .trim(),
   password: z
-    .string()
-    .min(6, { message: 'Your password must be at least 6 characters long' })
-    .max(100, { message: 'Your password must be at most 100 characters long' })
+    .string({ required_error: 'Password is required' })
+    .min(6, { message: 'Your password must be at least 6 characters' })
     .trim(),
 });
 
--- schema/register.ts
+++ schema/register.ts
@@ -3,32 +3,27 @@ import { z } from 'zod';
 const registerSchema = z
   .object({
     lastname: z
-      .string()
-      .min(2, { message: 'Your lastname must be at least 2 characters long' })
-      .max(50, { message: 'Your lastname must be at most 50 characters long' })
+      .string({ required_error: 'Lastname is required' })
+      .min(3, { message: 'Your lastname must be at least 3 characters' })
+      .max(50, { message: 'Your lastname must be at most 50 characters' })
       .trim(),
     firstname: z
-      .string()
-      .min(2, { message: 'Your firstname must be at least 2 characters long' })
-      .max(50, { message: 'Your firstname must be at most 50 characters long' })
+      .string({ required_error: 'Firstname is required' })
+      .min(3, { message: 'Your firstname must be at least 3 characters' })
+      .max(50, { message: 'Your firstname must be at most 50 characters' })
       .trim(),
-    email: z.string().email({ message: 'Invalid email address' }).trim(),
+    email: z
+      .string({ required_error: 'Email is required' })
+      .email({ message: 'Invalid email address' })
+      .trim(),
     password: z
-      .string()
-      .min(6, { message: 'Your password must be at least 6 characters long' })
-      .max(100, {
-        message: 'Your password must be at most 100 characters long',
-      })
+      .string({ required_error: 'Password is required' })
+      .min(6, { message: 'Your password must be at least 6 characters' })
       .trim(),
     passwordConfirm: z
-      .string()
+      .string({ required_error: 'Password confirm is required' })
       .min(6, {
-        message:
-          'Your password confirmation must be at least 6 characters long',
-      })
-      .max(100, {
-        message:
-          'Your password confirmation must be at most 100 characters long',
+        message: 'Your password confirmation must be at least 6 characters',
       })
       .trim(),
   })
