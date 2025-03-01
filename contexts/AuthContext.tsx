47cb37c69054625224809443c6117e0511a5f903
clement-jny <clementjaunay@free.fr>
2025-03-01 19:12:24 +0100

    refactor: update & improve auth context

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -6,24 +6,17 @@ import {
   ReactNode,
 } from 'react';
 import { firebaseConfig } from '@/utils';
-import { User } from 'firebase/auth';
-import { type TUser } from '@/types';
-// import { auth } from '@/services';
 
 type TAuthProps = {
-  user: User | null; // Connected User
-  // tUser: TUser | null;
-  userId: string; // Connected user uid
-  isLoading: boolean; // Chargement de l'utilisateur
-  // login: (email: string, password: string) => void;
-  // register: (email: string, password: string) => void;
-  // logout: () => void;
+  isAuthenticated: boolean;
+  authUid: string | undefined;
 };
 
-// Création du contexte avec une valeur par défaut `undefined`
-const Context = createContext<TAuthProps | undefined>(undefined);
+const Context = createContext<TAuthProps>({
+  isAuthenticated: false,
+  authUid: undefined,
+});
 
-// Hook personnalisé pour utiliser le contexte d'authentification
 const useAuth = () => {
   const context = useContext(Context);
 
@@ -32,53 +25,31 @@ const useAuth = () => {
   return context;
 };
 
-// Fournisseur de contexte pour gérer l'état d'authentification
 const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [user, setUser] = useState<User | null>(null);
-  // const [tUser, setTUser] = useState<TUser | null>(null);
-  const [userId, setUserId] = useState('');
-  const [isLoading, setIsLoading] = useState(true);
+  const [authUid, setAuthUid] = useState<string | undefined>(undefined);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
 
-  // useEffect(() => {
-  //   const unsubscribe = firebaseConfig.auth.onAuthStateChanged((_user) => {
-  //     console.log(`User ${_user ? 'connected' : 'disconnected'}`);
+  useEffect(() => {
+    const unsubscribe = firebaseConfig.auth.onAuthStateChanged((_user) => {
+      console.log(`User ${_user ? 'connected' : 'disconnected'}`);
 
-  //     console.log(_user);
+      if (_user) {
+        setIsAuthenticated(true);
+        setAuthUid(_user.uid);
+      } else {
+        setIsAuthenticated(false);
+        setAuthUid(undefined);
+      }
+    });
 
-  //     // setUser(_user);
-  //     // setUserId(_user ? _user.uid : '');
-  //     // setTUser(); // Need to get user from firestore with userId
-  //     // setIsLoading(isLoading ? false : true);
-  //   });
-
-  //   return () => unsubscribe();
-  // }, [firebaseConfig.auth]);
-
-  // useEffect(() => {
-  //   const unsubscribe = firebaseConfig.auth.onAuthStateChanged((_user) => {
-  //     setUser(_user);
-  //     setIsLoading(false);
-  //   });
-
-  //   return () => unsubscribe();
-  // }, []);
-
-  // const handleLogin = async (email: string, password: string) => {
-  //   try {
-  //     const response = await auth.login(email, password);
-  //     return response.user;
-  //   } catch (error) {
-  //     console.error('[handleLogin error] ==>', error);
-  //     return undefined;
-  //   }
-  // };
+    return () => unsubscribe();
+  }, [firebaseConfig.auth]);
 
   return (
-    <Context.Provider value={{ user, userId, isLoading }}>
+    <Context.Provider value={{ authUid, isAuthenticated }}>
       {children}
     </Context.Provider>
   );
 };
 
-// Export des composants pour une utilisation dans d'autres parties de l'application
 export const AuthContext = { useAuth, AuthProvider };
